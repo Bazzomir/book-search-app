0cd20bcc439a0670aa3f5fb589b6f810fa49d2e4
bazzomir <blagoj.jovanovski@outlook.com>
2024-07-11 05:12:47 +0200

    added card component and added new pages

---
+++ read-a-book/src/components/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="footer text-center text-lg-start">
+            <div className="text-center p-3">
+                Blagoj Jovanovski - Bazzomir © 2024 Copyright
+            </div>
+        </footer>
+    )
+}
--- read-a-book/src/components/component/Header.jsx
+++ read-a-book/src/components/component/Header.jsx
@@ -4,7 +4,7 @@ import logoIcon from '../../assets/image/logo-icon.png'
 import search from '../../assets/image/search.png'
 
 
-export default function HeaderNavigation() {
+export default function Header() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
@@ -18,13 +18,13 @@ export default function HeaderNavigation() {
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="d-flex w-100 justify-content-between px-1 px-md-3 px-lg-5">
+                    <ul className="d-flex w-100 justify-content-between px-1 px-md-3 px-lg-5 mb-0">
                         <a className="navbar-brand d-none d-lg-block" href="#">
                             <img src={logo} alt="Logo" className="d-inline-block navbar-brand__logo" />
                         </a>
-                        <ul className="d-flex justify-content-center align-items-center gap-lg-5">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <ul className="d-flex justify-content-center align-items-lg-center gap-3 gap-lg-5 flex-column-reverse flex-lg-row">
+                            <li className="nav-item dropdown me-lg-4">
+                                <a className="nav-link dropdown-toggle text-22" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Category
                                 </a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -33,7 +33,7 @@ export default function HeaderNavigation() {
                                     <li><a className="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
                             </li>
-                            <form className="d-flex" role="search">
+                            <form className="d-flex mt-3 mt-lg-0 ms-lg-4" role="search">
                                 <input className="form-control search-bar" type="search" placeholder="Find your favorite book..." aria-label="Search" />
                                 <button className="btn search-btn" type="submit">
                                     <img src={search} alt="Search icon" width="24" height="24" />
