260960bf47e865365985603f9bb1a6881e191413
bazzomir <blagoj.jovanovski@outlook.com>
2024-07-13 19:27:18 +0200

    updated Ruti.js, Header.jsx and Homepage.jsx... added NotFound.jsx page

--- read-a-book/src/components/Ruti.js
+++ read-a-book/src/components/Ruti.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './component/pages/Homepage';
+import NotFound from './component/pages/NotFound';
 
 export default function Ruti() {
     return (
         <Routes>
             <Route path="/" element={<Homepage />} />
-            <Route path="" element={""} />
-            <Route path="" element={""} />
-            <Route path="" element={""} />
+            <Route path="/genre/:genre" element={<Homepage />} />
+            <Route path="*" element={<NotFound />} />
+            {/* <Route path="" element={""} />
+            <Route path="" element={""} /> */}
         </Routes>
     )
 }
--- read-a-book/src/components/component/pages/Homepage.jsx
+++ read-a-book/src/components/component/pages/Homepage.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Card from '../Card';
 
 export default function Homepage() {
-
+    const { genre } = useParams();
     const [data, setData] = useState([]);
 
     const getData = async () => {
         try {
-            const response = await fetch('books.json', {
+            const response = await fetch('/books.json', {
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
@@ -15,8 +16,11 @@ export default function Homepage() {
             });
 
             const json = await response.json();
-            setData(json);
-            console.table("Json Data:", json)
+            if (genre) {
+                setData(json.filter(book => book.genre === genre));
+            } else {
+                setData(json);
+            }
 
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -25,10 +29,10 @@ export default function Homepage() {
 
     useEffect(() => {
         getData();
-    }, []);
+    }, [genre]);
 
     return (
-        <div className="container">
+        <div className="container vh-100">
             <div className="row py-5">
                 <div className="col-12 p-2">
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 d-flex justify-content-around align-items-center gap-5 m-0 py-2">
@@ -37,7 +41,6 @@ export default function Homepage() {
                                 <Card key={index} book={book} />
                             ))
                         ) : (
-                            //Tuka treba Page NotFound da bidi
                             <p>Loading data...</p>
                         )}
                     </div>
