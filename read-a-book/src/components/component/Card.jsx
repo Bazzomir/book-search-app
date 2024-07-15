0f2c05551bdc3b9d35f7ed9c1fd8466922cb071d
bazzomir <blagoj.jovanovski@outlook.com>
2024-07-15 05:02:42 +0200

    change

--- read-a-book/src/components/component/Card.jsx
+++ read-a-book/src/components/component/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Card({ book }) {
     return (
         <div className="card px-0">
-            <div className="card-header">{book.id}</div>
+            {/* <div className="card-header">{book.id}</div> */}
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">Title: {book.title}</li>
                 <li className="list-group-item">Author: {book.author}</li>
