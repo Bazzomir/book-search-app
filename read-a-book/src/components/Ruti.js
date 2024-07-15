eecca78139242346f3b496768eb12b451c299315
bazzomir <blagoj.jovanovski@outlook.com>
2024-07-15 05:02:02 +0200

    delete...

--- read-a-book/src/components/Ruti.js
+++ read-a-book/src/components/Ruti.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './component/pages/Homepage';
-import NotFound from './component/pages/NotFound';
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Homepage from './component/pages/Homepage';
+// import NotFound from './component/pages/NotFound';
 
-export default function Ruti() {
-    return (
-        <Routes>
-            <Route path="/" element={<Homepage />} />
-            {/* <Route path="/genre/:genre" element={<Homepage />} /> */}
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    )
-}
+// export default function Ruti() {
+//     return (
+//         <Routes>
+//             <Route path="/" element={<Homepage />} />
+//             {/* <Route path="/genre/:genre" element={<Homepage />} /> */}
+//             <Route path="*" element={<NotFound />} />
+//         </Routes>
+//     )
+// }
