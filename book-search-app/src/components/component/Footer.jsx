2ee8e3372892f5593b274c79689be31b1d09772b
bazzomir <bazzomir97@gmail.com>
2025-03-05 04:07:06 +0100

    updated footer

--- book-search-app/src/components/component/Footer.jsx
+++ book-search-app/src/components/component/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer className="footer text-center text-lg-start mt-auto">
-            <div className="text-center p-3">
-                Blagoj Jovanovski - Bazzomir © 2024 Copyright
-            </div>
+            <div className="text-center p-3"> Bazzomir © 2025</div>
         </footer>
     )
 }
